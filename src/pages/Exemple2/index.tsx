a72c523a70461a3ead102a4a8d17d0fdbce1e055
ewertonbn <ewertonbn.dev@gmail.com>
2023-05-24 12:42:39 -0300

    feat: add pause in interval

--- src/pages/Exemple2/index.tsx
+++ src/pages/Exemple2/index.tsx
@@ -1,15 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import videoFile1 from '../../assets/itau.mp4'
 import videoFile2 from '../../assets/tramontina.mp4'
 import videoFile3 from '../../assets/personnalite.mp4'
-
-/*
-  Ter duas tags vídeos com case atual e a prévia do próximo
-  quando pegar o primeiro item do array, pegar junto o segundo e carregar na prévia
-  Após 5 segundos o case atual é substituído pela prévia e assim sucessivamente.
-*/
 
 interface CaseProps {
   id: string
@@ -42,45 +36,92 @@ const casesList: Array<CaseProps> = [
 export function Exemple2() {
   const [currentCase, setCurrentCase] = useState<CaseProps>(casesList[0])
   const [nextCase, setNextCase] = useState<CaseProps>(casesList[1])
+
   const [current, setCurrent] = useState<number>(1)
   const [next, setNext] = useState<number>(2)
 
+  const [isActive, setIsActive] = useState(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  const [pause, setPause] = useState(false)
+
   useEffect(() => {
     const interval = setInterval(() => {
-      if (current <= casesList.length) {
-        setCurrent((state) => state + 1)
-        setNext((state) => state + 1)
+      if (!pause) {
+        if (current <= casesList.length) {
+          setCurrent((state) => state + 1)
+          setNext((state) => state + 1)
 
-        const newCurrentCase = casesList[current]
-        const newNextCase = casesList[next]
+          const newCurrentCase = casesList[current]
+          const newNextCase = casesList[next]
 
-        setCurrentCase(newCurrentCase)
-        setNextCase(newNextCase)
-        console.log(current + ' - ' + next)
+          setCurrentCase(newCurrentCase)
+          setNextCase(newNextCase)
+          console.log(current + ' - ' + next)
+        }
+
+        if (current === casesList.length - 1) {
+          setCurrent(0)
+        }
+
+        if (next === casesList.length - 1) {
+          setNext(0)
+        }
       }
-
-      if (current === casesList.length - 1) {
-        setCurrent(0)
-      }
-
-      // Zera contador preview para funcionar o lopping comparando quando chega na última posição
-      if (next === casesList.length - 1) {
-        setNext(0)
-      }
-    }, 5000)
+    }, 6000)
     return () => clearInterval(interval)
-  }, [current, next])
+  }, [current, next, pause])
+
+  function handleOnFocusedVideo() {
+    setIsActive(true)
+    videoRef.current?.pause()
+    setPause(true)
+  }
+
+  function handleNotFocusedVideo() {
+    setIsActive(false)
+    videoRef.current?.play()
+    setPause(false)
+  }
 
   return (
     <>
       <Link to="/">Home</Link>
       <div className="videoContainer">
-        <video key={currentCase.id} loop playsInline muted autoPlay controls>
-          <source type="video/mp4" src={currentCase.src} />
-        </video>
-        <video key={nextCase.id} loop playsInline muted autoPlay controls>
-          <source type="video/mp4" src={nextCase.src} />
-        </video>
+        <div>
+          <video
+            key={currentCase.id}
+            loop
+            playsInline
+            muted
+            autoPlay
+            controls
+            ref={videoRef}
+            onMouseEnter={handleOnFocusedVideo}
+            onMouseLeave={handleNotFocusedVideo}
+          >
+            <source type="video/mp4" src={currentCase.src} />
+          </video>
+          <div
+            className={`modal ${isActive ? 'active' : ''}`}
+            onMouseEnter={handleOnFocusedVideo}
+            onMouseLeave={handleNotFocusedVideo}
+          >
+            <h1>{currentCase.name}</h1>
+            <a
+              href={`/case/${currentCase.slug}`}
+              className={`${isActive ? 'active' : ''}`}
+            >
+              Acessar {currentCase.name}
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <video key={nextCase.id} loop playsInline muted autoPlay controls>
+            <source type="video/mp4" src={nextCase.src} />
+          </video>
+        </div>
       </div>
     </>
   )
